b7bdf33f455a57eae6312dfedc95ce53365e6197
tanka2u <ptanka123@gmail.com>
2024-08-17 23:44:41 +0200

    search optimization

---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,28 @@
+import { useRef, useState } from "react";
+
+export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
+  const [searchTerm, setSearchItem] = useState("");
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  function handleChange(event) {
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchItem(event.target.value);
+    }, 500);
+  }
+  return (
+    <div className="searchable-list">
+      <input type="search" placeholder="Search" onChange={handleChange} />
+      <ul>
+        {searchResults.map((item) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
